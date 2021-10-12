b429334215ef07dfbe4ad83d0a112c4487d0047e
Navid Ali <43358902+navidali@users.noreply.github.com>
2021-10-12 00:52:26 -0400

    skeletonization

---
+++ ClassicalMLTechnique/main.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+def main():
+  
+    img = cv2.imread("C:/Users/Glados/Desktop/Peanut_MR/test/masks_pixel_gt//Peanut_640x480_DPI120/GT_Peanut_T003_L003_2017.06.15_080543_EED_DPI120.png", 1)
+    img = cv2.bitwise_not(img)
+    blur = cv2.GaussianBlur(img, (5, 5), 0)
+    thinned = cv2.ximgproc.thinning(cv2.cvtColor(blur, cv2.COLOR_RGB2GRAY), thinningType=cv2.ximgproc.THINNING_ZHANGSUEN)
+
+    cv2.imshow("skeleton", thinned)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
